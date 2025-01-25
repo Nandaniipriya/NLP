35cba54fc0ade39c1515ffb615b9e076ea0067c7
Nandani <126315099+Nandaniipriya@users.noreply.github.com>
2025-01-25 20:32:56 +0000

    flask

---
+++ Flask/flask/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+### WSGI Application
+app=Flask(__name__)
+@app.route("/")
+def welcome():
+    return "Welcome to this flask"
+
+if __name__=="__main__":
+    app.run(debug=True)
