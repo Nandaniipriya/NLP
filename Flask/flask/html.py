5eafae4c18c70980b949a80d6c77622cb95b4f1a
Nandani <126315099+Nandaniipriya@users.noreply.github.com>
2025-01-26 13:25:43 +0000

    html

--- Flask/flask/html.py
+++ Flask/flask/html.py
@@ -7,6 +7,9 @@ def welcome():
 @app.route("/index",methods=['GET'])
 def index():
     return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__=="__main__":
